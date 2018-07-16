ae8ca720d2a14ea7d32a71e9243cf95c1a0e46a5
Jack Baron <jackmbaron@gmail.com>
2018-07-16 22:59:00 +0100

    Download zip to the right place

--- download.py
+++ download.py
@@ -1,4 +1,7 @@
 import requests
+import zipfile
+import html
+import io
 
 api = 'https://beatsaver.com/api/songs/new/{}'
 dl = 'https://beatsaver.com/download/{}'
@@ -10,3 +13,23 @@ def get_total():
 def fetch_single(start: int):
     resp = requests.get(api.format(start)).json()
     return resp['songs']
+
+def download(song):
+    key = song['key']
+    name = song['name']
+
+    # If we know we've downloaded a song already, skip it
+    if check_json(key):
+        return
+
+    # *unzips*
+    zip_file = requests.get(dl.format(key)).content
+    try:
+        with zipfile.ZipFile(io.BytesIO(zip_file)) as song_zip:
+            song_zip.extractall('CustomSongs/{}'.format(song['key']))
+            
+        # Save the fact we've downloaded this song
+        write_json(key)
+        print('Downloading {}'.format(name))
+    except:
+        print('Failed to download {}. An Error occoured'.format(html.unescape(name)))
